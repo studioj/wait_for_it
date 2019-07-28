279b8f96f461588c9cbcce77583c206c8c516853
studioj <neefsj@gmail.com>
2019-07-28 14:49:25 +0200

    adding project files making coverage run

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+_locals = {}
+with open("wait_for_it/_version.py") as fp:
+    exec(fp.read(), None, _locals)
+version = _locals["__version__"]
+
+longdesc = """
+This is a library for letting you python code wait for a certain action top complete"""
+
+setup(
+    name="wait_for_it",
+    version=version,
+    description="SSH2 protocol library",
+    long_description=longdesc,
+    author="Jef Neefs",
+    author_email="neefsj@gmail.com",
+    url="https://github.com/studioj/wait_for_it/",
+    packages=["wait_for_it"],
+    license="LGPL",
+    platforms="Posix; MacOS X; Windows",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: "
+        "GPL v3",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    # install_requires=[],
+)
---
+++ tests/test_wait_for_it_initial.py
@@ -0,0 +1,5 @@
+import wait_for_it
+
+
+def test_wait_for_it_has_a_version():
+    assert wait_for_it.__version__
