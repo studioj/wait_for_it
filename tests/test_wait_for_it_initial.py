81aff91ac48ba1f35c6584ee5e346e7d197d2b0d
studioj <neefsj@gmail.com>
2019-07-31 00:20:38 +0200

    first tdd pass

--- tests/test_wait_for_it_initial.py
+++ tests/test_wait_for_it_initial.py
@@ -1,5 +1,16 @@
+from mock import patch
+
 import wait_for_it
 
 
 def test_wait_for_it_has_a_version():
     assert wait_for_it.__version__
+
+
+def test_wait_for_it_to_be_true_immediately_returns_when_func_evals_to_true():
+    def foo():
+        return True
+
+    with patch("wait_for_it.time.sleep") as mocked_sleep:
+        wait_for_it.to_be_true(foo)
+        mocked_sleep.assert_not_called()
--- wait_for_it/__init__.py
+++ wait_for_it/__init__.py
@@ -1,1 +1,5 @@
 from wait_for_it._version import __version__, __version_info__
+
+
+def to_be_true(func):
+    pass
