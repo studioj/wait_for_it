ab303b29ef493fcdc2f46d13ad2a4d3f3c2979e5
studioj <neefsj@gmail.com>
2019-08-03 23:21:29 +0200

    making sure to be true executes the variable function and sleeps

--- tests/test_wait_for_it_initial.py
+++ tests/test_wait_for_it_initial.py
@@ -1,4 +1,4 @@
-from mock import patch
+from mock import patch, MagicMock
 
 import wait_for_it
 
@@ -14,3 +14,18 @@ def test_wait_for_it_to_be_true_immediately_returns_when_func_evals_to_true():
     with patch("wait_for_it.time.sleep") as mocked_sleep:
         wait_for_it.to_be_true(foo)
         mocked_sleep.assert_not_called()
+
+
+def test_to_be_true_sleeps_once_when_the_func_returns_only_after_the_second_try():
+    foo = MagicMock()
+    foo.side_effect = [False, True]
+    with patch("wait_for_it.time.sleep") as mocked_sleep:
+        wait_for_it.to_be_true(foo)
+        mocked_sleep.assert_called_with(0.01)
+
+
+def test_to_be_true_calls_the_passed_function_obejct():
+    foo = MagicMock()
+    with patch("wait_for_it.time.sleep") as mocked_sleep:
+        wait_for_it.to_be_true(foo)
+        foo.assert_called_once()
--- wait_for_it/__init__.py
+++ wait_for_it/__init__.py
@@ -1,5 +1,10 @@
+import time
+
 from wait_for_it._version import __version__, __version_info__
 
 
 def to_be_true(func):
-    pass
+    while True:
+        if func():
+            return True
+        time.sleep(0.01)
