21bf1e5981224496683de6212751f8efb6c0f7b0
studioj <neefsj@gmail.com>
2019-08-03 23:55:28 +0200

    raise timeouterror when condition is not met removing python2 support since timeouterror is not supported

--- wait_for_it/__init__.py
+++ wait_for_it/__init__.py
@@ -4,7 +4,12 @@ from wait_for_it._version import __version__, __version_info__
 
 
 def to_be_true(func):
+    start = time.time()
     while True:
-        if func():
+        result = func()
+        if result:
             return True
+        if time.time() > start + 10:
+            msg = "expected something that evaluates to True, but got %s instead" % str(result)
+            raise TimeoutError(msg)
         time.sleep(0.01)
