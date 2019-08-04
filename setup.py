ffc84050a05b15cff3e517ae18b4a802520cb658
studioj <neefsj@gmail.com>
2019-08-04 16:16:15 +0200

    renaming package

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-from waitforit import __version__
+from wait_for_it_to import __version__
 
 version = __version__
 
@@ -9,15 +9,15 @@ longdesc = """
 This is a library for letting you python code wait for a certain action top complete"""
 
 setup(
-    name="waitforit",
+    name="wait_for_it_to",
     version=version,
-    description="SSH2 protocol library",
+    description="helper library which waits",
     long_description=longdesc,
     author="Jef Neefs",
     author_email="neefsj@gmail.com",
     url="https://github.com/studioj/wait_for_it/",
-    packages=["waitforit"],
-    license="LGPL",
+    packages=["wait_for_it_to"],
+    license="GPL",
     platforms="Posix; MacOS X; Windows",
     classifiers=[
         "Development Status :: 1 - Planning",
