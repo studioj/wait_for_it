1d9f3961d8a64c8b19c7c9608c27a44d8a779045
studioj <neefsj@gmail.com>
2019-08-05 00:08:41 +0200

    Update documentation (#4)
    * adding documentation for be true
    * updating the long description

--- wait_for_it_to/__init__.py
+++ wait_for_it_to/__init__.py
@@ -4,11 +4,17 @@ from wait_for_it_to._version import __version__, __version_info__
 
 
 def be_true(func):
+    """
+    waits until func evaluates to True
+    raises an exception when the timeout expires
+
+    :param func: an executable object
+    """
     start = time.time()
     while True:
         result = func()
         if result:
-            return True
+            return
         if time.time() > start + 10:
             msg = "expected something that evaluates to True, but got %s instead" % str(result)
             raise TimeoutError(msg)
