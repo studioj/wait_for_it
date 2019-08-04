d2c83cd9d120432a6e21a88536c21a9131c7eb62
studioj <neefsj@gmail.com>
2019-08-04 20:46:53 +0200

    Release_pipeline_final
    * adding travis and pypi release info

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-from wait_for_it import __version__
+from wait_for_it_to import __version__
 
 version = __version__
 
@@ -9,21 +9,20 @@ longdesc = """
 This is a library for letting you python code wait for a certain action top complete"""
 
 setup(
-    name="wait_for_it",
+    name="wait_for_it_to",
     version=version,
-    description="SSH2 protocol library",
+    description="helper library which waits",
     long_description=longdesc,
     author="Jef Neefs",
     author_email="neefsj@gmail.com",
     url="https://github.com/studioj/wait_for_it/",
-    packages=["wait_for_it"],
-    license="LGPL",
+    packages=["wait_for_it_to"],
+    license="GPL",
     platforms="Posix; MacOS X; Windows",
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: "
-        "GPL v3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Topic :: Utilities",
         "Programming Language :: Python",
--- tests/test_wait_for_it_initial.py
+++ tests/test_wait_for_it_initial.py
@@ -2,36 +2,36 @@ import unittest
 
 from mock import patch, MagicMock
 
-import wait_for_it
+import wait_for_it_to
 
 
 class TestWaitForItToBeTrue(unittest.TestCase):
 
-    def test_wait_for_it_has_a_version(self):
-        assert wait_for_it.__version__
+    def test_wait_for_it_to_has_a_version(self):
+        assert wait_for_it_to.__version__
 
-    def test_wait_for_it_to_be_true_immediately_returns_when_func_evals_to_true(self):
+    def test_wait_for_it_to_to_be_true_immediately_returns_when_func_evals_to_true(self):
         foo = MagicMock()
         foo.return_value = True
-        with patch("wait_for_it.time.sleep") as mocked_sleep:
-            wait_for_it.to_be_true(foo)
+        with patch("wait_for_it_to.time.sleep") as mocked_sleep:
+            wait_for_it_to.be_true(foo)
             mocked_sleep.assert_not_called()
 
     def test_to_be_true_sleeps_once_when_the_func_returns_only_after_the_second_try(self):
         foo = MagicMock()
         foo.side_effect = [False, True]
-        with patch("wait_for_it.time.sleep") as mocked_sleep:
-            wait_for_it.to_be_true(foo)
+        with patch("wait_for_it_to.time.sleep") as mocked_sleep:
+            wait_for_it_to.be_true(foo)
             mocked_sleep.assert_called_with(0.01)
 
     def test_to_be_true_calls_the_passed_function_obejct(self):
         foo = MagicMock()
-        with patch("wait_for_it.time.sleep") as mocked_sleep:
-            wait_for_it.to_be_true(foo)
+        with patch("wait_for_it_to.time.sleep") as mocked_sleep:
+            wait_for_it_to.be_true(foo)
             foo.assert_called_once()
 
     def test_to_be_true_raises_timeout_error_when_timeout_has_passed(self):
         foo = MagicMock()
         foo.return_value = False
-        with patch("wait_for_it.time.sleep") as mocked_sleep:
-            self.assertRaises(TimeoutError, wait_for_it.to_be_true, foo)
+        with patch("wait_for_it_to.time.sleep") as mocked_sleep:
+            self.assertRaises(TimeoutError, wait_for_it_to.be_true, foo)
