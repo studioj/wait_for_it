2d755ecf172f2273e0905cc4086cdac4c58aeebe
studioj <neefsj@gmail.com>
2019-08-23 01:42:15 +0200

    Wait for it 19 (#25)
    * waiting for a real True
    * waiting for a real False
    * refactored out to_be_equal

--- wait_for_it_to/__init__.py
+++ wait_for_it_to/__init__.py
@@ -5,7 +5,7 @@ from wait_for_it_to._version import __version__, __version_info__
 
 def be_true(func, timeout=10):
     """
-    waits until func evaluates to True
+    waits until func returns True
     raises an exception when the timeout expires
 
     :param timeout: a timeout in seconds
@@ -16,22 +16,14 @@ def be_true(func, timeout=10):
     >>>
     >>>wait_for_it_to.be_true(foo)
     >>>wait_for_it_to.be_true(foo, timeout=5)
-    
+
     """
-    start = time.time()
-    while True:
-        result = func()
-        if result:
-            return True
-        if time.time() > start + timeout:
-            msg = "expected something that evaluates to True, but got %s instead" % str(result)
-            raise TimeoutError(msg)
-        time.sleep(0.01)
+    be_equal(func, True, timeout)
 
 
 def be_false(func, timeout=10):
     """
-    waits until func evaluates to False
+    waits until func returns False
     raises an exception when the timeout expires
 
     :param timeout: a timeout in seconds
@@ -43,12 +35,29 @@ def be_false(func, timeout=10):
     >>>wait_for_it_to.be_false(foo)
     >>>wait_for_it_to.be_false(foo, timeout=5)
     """
+    be_equal(func, False, timeout)
+
+
+def be_equal(func, expected_value, timeout=10):
+    """
+    waits until func is equal to expected_value
+    raises an exception when the timeout expires
+
+    :param expected_value: any value func should evaluate to
+    :param timeout: a timeout in seconds
+    :param func: an executable object
+
+    >>>def foo():
+    >>>  return "any object"
+    >>>
+    >>>wait_for_it_to.be_equal(foo, "any object")
+    >>>wait_for_it_to.be_equal(foo, "any object", timeout=5)
+    """
     start = time.time()
-    while True:
-        result = func()
-        if not result:
-            return False
+    result = func()
+    while result != expected_value:
         if time.time() > start + timeout:
             msg = "expected something that evaluates to True, but got %s instead" % str(result)
             raise TimeoutError(msg)
         time.sleep(0.01)
+        result = func()
