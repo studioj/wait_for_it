efcb10faec5c876a8eb2b11c3241b38f7807ac4d
studioj <22102283+studioj@users.noreply.github.com>
2023-05-19 22:58:00 +0200

    adding docs (#174)
    * adding docs
    
    * black the repo
    
    * add pytest cov
    
    * drop 37
    
    * cleanup readme
    
    * adding codecov

--- tests/test_wait_for_it_to_not_raise_an_exception.py
+++ tests/test_wait_for_it_to_not_raise_an_exception.py
@@ -14,7 +14,9 @@ except NameError:
 
 
 class TestWaitForItToNotRaiseAnException(unittest.TestCase):
-    def test_wait_for_it_not_to_raise_an_exception_returns_immediatly_when_no_exception_raised(self):
+    def test_wait_for_it_not_to_raise_an_exception_returns_immediatly_when_no_exception_raised(
+        self,
+    ):
         func = MagicMock()
 
         start = time.time()
@@ -28,7 +30,9 @@ class TestWaitForItToNotRaiseAnException(unittest.TestCase):
 
         self.assertRaises(TimeoutError, wait_for_it_to.not_raise_an_exception, func, 1)
 
-    def test_raises_a_timeout_error_when_func_keeps_raising_exceptions_which_are_equal_to_the_sentinel_exception(self):
+    def test_raises_a_timeout_error_when_func_keeps_raising_exceptions_which_are_equal_to_the_sentinel_exception(
+        self,
+    ):
         def func():
             raise RuntimeError(EXCEPTION_STRING)
 
@@ -38,7 +42,13 @@ class TestWaitForItToNotRaiseAnException(unittest.TestCase):
         def func():
             raise RuntimeError(EXCEPTION_STRING)
 
-        self.assertRaises(RuntimeError, wait_for_it_to.not_raise_an_exception, func, 1, EnvironmentError)
+        self.assertRaises(
+            RuntimeError,
+            wait_for_it_to.not_raise_an_exception,
+            func,
+            1,
+            EnvironmentError,
+        )
 
     def test_returns_if_succesfull(self):
         def func(num):
